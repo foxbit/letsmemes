29487322769b2a87fcbdb2dcd51371dea73dc2ae
angelorosa <angelorosa@gmail.com>
2021-03-07 18:44:31 -0300

    Limpando código

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,9 @@ let resultadoDown = document.getElementById('divResultadoDown');
 let textBoxUp = document.getElementById('textBoxUp');
 let textBoxDown = document.getElementById('textBoxDown');
 let btnDownload = document.getElementById('downloadButton');
+let inputImage = document.getElementById('inputUploadImage')
+
+/* Lista de memes e seletor */
 
 
 function selecionarFundo() {
@@ -17,6 +20,21 @@ opcaoImagem.forEach(function (elemento) {
     elemento.addEventListener('input', selecionarFundo);
 })
 
+/* Upload de foto */
+
+window.addEventListener('load', function(){
+    document.getElementById('inputUploadImage').addEventListener('change', function(){
+        if (this.files && this.files[0]) {
+            var img = document.querySelector('img');
+            img.src = URL.createObjectURL(this.files[0]);
+            resultadoBG.style.backgroundImage = `url(${img.src})`;
+        }
+    }
+    );
+});
+
+/* Inputs de textos */
+
 textBoxUp.addEventListener('input', function () {
     resultadoUp.innerHTML = "<span class='textMeme'>" + this.value + "<span>";
 })
@@ -25,8 +43,11 @@ textBoxDown.addEventListener ('input', function () {
     resultadoDown.innerHTML = "<span class='textMeme'>" + this.value + "<span>";
 })
 
+
+/* Download */
+
 function download() {
-    domtoimage.toPng(resultadoBG, /*{height: 800, width:800}* este parâmtro está criando uma imagem maior porém os elementos não estao acompanhando o tamanho do canvas*/)
+    domtoimage.toPng(resultadoBG, /*{height: 800, width:800}*/)
         .then(function (urlsaida) {
             let link = document.createElement('a');
             link.download = 'saida.png';
